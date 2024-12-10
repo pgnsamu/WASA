0282810eca50b1c6be0dbb8e309e38bf40c57e12
Samuele <pgn.samu@gmail.com>
2024-12-10 15:23:31 +0100

    fix return type

--- service/api/getUsers.go
+++ service/api/getUsers.go
@@ -9,7 +9,7 @@ import (
 
 // getHelloWorld is an example of HTTP endpoint that returns "Hello world!" as a plain text
 func (rt *_router) getUsers(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	w.Header().Set("content-type", "text/plain")
+	w.Header().Set("content-type", "application/json")
 	name, err := rt.db.GetUsersDB()
 	if err != nil {
 		return
